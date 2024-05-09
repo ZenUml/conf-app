8253a12120759fdd639321f80c78a36afedfe642
MrCoder <eagle.xiao@gmail.com>
2024-05-09 21:40:17 +1000

    Set deadline as 1st March 2025.

--- src/components/Notice/index.vue
+++ src/components/Notice/index.vue
@@ -6,7 +6,7 @@
           <svg class="fill-current h-6 w-6 text-orange-500 mr-4" viewBox="0 0 24 24" role="presentation"><path d="M13.31 5.343l7.359 13.17A1 1 0 0119.796 20H4.204a1 1 0 01-.873-1.488l7.36-13.169a1.5 1.5 0 012.618 0zM12 8.5a1.091 1.091 0 00-1.081 1.239l.513 3.766a.573.573 0 001.136 0l.513-3.766A1.091 1.091 0 0012 8.5zm0 8.63a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" fill="currentColor" fill-rule="evenodd"></path></svg>
         </div>
         <div>
-          <p class="font-bold">Notice: We’re soon introducing a limit on the number of macros clients on the free tier can create and modify.</p>
+          <p class="font-bold">Notice: Starting March 1, 2025, we’re introducing a limit on the number of macros that clients on the free tier can create and modify.</p>
           <p class="">
             Your organization has already created over {{ numberOfMacros }} macros!
             <a @click="onUpgradeLinkClicked" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://zenuml.com/upgrade/" target="_blank">Upgrade for unlimited creation and modification</a>, or

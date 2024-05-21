b832097b810de74d1fb02405f16b8b4a8af2a9fb
unimu-cic <125171539+unimu-cic@users.noreply.github.com>
2024-05-21 21:02:40 +0800

    fix: unit test

--- src/components/Header/Header.spec.ts
+++ src/components/Header/Header.spec.ts
@@ -1,10 +1,10 @@
-import { mount } from '@vue/test-utils'
+import {mount} from '@vue/test-utils'
 import Header from '@/components/Header/Header.vue'
 import {DiagramType} from "@/model/Diagram/Diagram";
 import store from "@/model/store2/";
 
 describe('Header', () => {
-  it('should render correctly', (done) => {
+  it('should render correctly', async (done) => {
     store.commit('updateDiagramType', DiagramType.Sequence);
     const headerWrapper = mount(Header, {
       global: {
@@ -12,19 +12,22 @@ describe('Header', () => {
       }
     })
     // pre-condition
-    const sequenceButton = headerWrapper.find({ref: 'btn-sequence'});
+    const sequenceButton = headerWrapper.find('#btn-sequence');
     expect(sequenceButton.classes('bg-white')).toBeTruthy();
-    const mermaidButton = headerWrapper.find({ref: 'btn-mermaid'});
+    const mermaidButton = headerWrapper.find('#btn-mermaid');
     expect(mermaidButton.classes('bg-white')).toBeFalsy();
 
     // click to switch to mermaid
     // @ts-ignore
     expect(headerWrapper.vm.$store.state.diagram.diagramType).toBe(DiagramType.Sequence);
-    mermaidButton.trigger('click');
+    await mermaidButton.trigger('click');
+    await headerWrapper.vm.$nextTick()
+
     // @ts-ignore
     expect(headerWrapper.vm.$store.state.diagram.diagramType).toBe(DiagramType.Mermaid);
-    headerWrapper.vm.$nextTick(() => {
-      expect(mermaidButton.classes('bg-white')).toBeTruthy();
-    })
+
+    setTimeout(() => {
+      expect(mermaidButton.classes('bg-white')).toBeTruthy()
+    }, 500)
   })
 })

d0349d3078cf2699d798d50fcd0e3932f7d68156
Yanhui Li <whimet@gmail.com>
2024-10-08 19:51:28 +1100

    ZEN-109 Report custom content in each space on daily basis

--- src/services/CustomContentReportingService.ts
+++ src/services/CustomContentReportingService.ts
@@ -6,25 +6,32 @@ const yesterday = () => new Date(Date.now() - 86400000);
 
 export async function reportCustomContent() {
   try {
-    const customContentReport = await globals.apWrapper.getAppProperty('CustomContentReport');
+    const space = (await globals.apWrapper._getCurrentSpace()).key;
+    let property = await globals.apWrapper.getAppProperty('CustomContentReport');
 
-    if(!customContentReport || new Date(customContentReport.lastUpdated) < yesterday() ) {
-      console.debug('start another reporting since the last CustomContentReport:', customContentReport);
+    if(!property || !property[space] || new Date(property[space].lastUpdated) < yesterday() ) {
+      console.debug(`start another reporting since the last CustomContentReport in space ${space}:`, property);
 
-      const result = await searchCustomContent();
-      console.debug(`reportCustomContent - statistics of custom content:`, result);
+      const result = await searchCustomContent(space);
+      console.debug(`reportCustomContent - statistics of custom content in space ${space}:`, result);
       trackEvent(`${JSON.stringify(result)}`, 'reportCustomContent', 'info');
 
-      await globals.apWrapper.setAppProperty('CustomContentReport', {lastUpdated: new Date().toISOString()})
+      await updateAppProperty(property, space);
     }
   } catch(e) {
     console.error('Error on reportCustomContent', e);
   }
 }
 
-async function searchCustomContent() {
+async function updateAppProperty(property: any, space: string) {
+  const p = Object.assign({}, property);
+  p.lastUpdated = new Date().toISOString();
+  p[space] = p.lastUpdated;
+  await globals.apWrapper.setAppProperty('CustomContentReport', p);
+}
+
+async function searchCustomContent(space: string) {
   let total = 0, sequence = 0, graph = 0, openapi = 0, mermaid = 0, unknown = 0;
-  const space = (await globals.apWrapper._getCurrentSpace()).key;
   const typesFilter = globals.apWrapper.buildTypesClauseFilter();
   const spacesFilter = `space in (${space})`;
   const searchUrl = `/rest/api/content/search?expand=body.raw&cql=${spacesFilter} and (${typesFilter})`;
@@ -51,6 +58,6 @@ async function searchCustomContent() {
     await globals.apWrapper.requestAllPaginatedData(searchUrl, consumer);
     return {space, total, sequence, graph, openapi, mermaid, unknown};
   } catch (e) {
-    console.error('searchCustomContent', e);
+    console.error('Error on searchCustomContent', e);
   }
 }

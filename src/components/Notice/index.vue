a1fdc39a3c553cca784388b03832dd5355f57583
MrCoder <eagle.xiao@gmail.com>
2024-11-27 20:12:27 +1100

    refactor(upgrade-notice): improve code structure and clarity

--- src/components/Notice/index.vue
+++ src/components/Notice/index.vue
@@ -1,16 +1,14 @@
 <template>
-  <div v-show="showNotice">
+  <div v-show="actionRequired">
     <div class="bg-orange-50 border-t-4 border-orange-500 rounded-b text-orange-700 px-4 py-3 shadow-md" role="alert">
       <div class="flex">
         <div class="py-1">
           <svg class="fill-current h-6 w-6 text-orange-500 mr-4" viewBox="0 0 24 24" role="presentation"><path d="M13.31 5.343l7.359 13.17A1 1 0 0119.796 20H4.204a1 1 0 01-.873-1.488l7.36-13.169a1.5 1.5 0 012.618 0zM12 8.5a1.091 1.091 0 00-1.081 1.239l.513 3.766a.573.573 0 001.136 0l.513-3.766A1.091 1.091 0 0012 8.5zm0 8.63a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" fill="currentColor" fill-rule="evenodd"></path></svg>
         </div>
         <div>
-          <p class="font-bold">Notice: Starting March 1, 2025, we’re introducing a limit on the number of macros that clients on the free tier can create and modify in one space.</p>
-          <p class="">
-            Your organization has already created {{ numberOfMacros }} macros in this space!
-            <a @click="onUpgradeLinkClicked" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://zenuml.com/upgrade/" target="_blank">Upgrade for unlimited creation and modification</a>, or
-            <a @click="onContactLinkClicked" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://zenuml.com/docs/about/contact-us#contact-us" target="_blank">contact us for support</a>.
+          <p class="font-bold">Warning: Your space contains {{macrosCreated}} macros, but will be limited to {{macrosLimit}} macros on March 1, 2025 under the free plan.</p>
+          <p>To continue creating new macros or modifying existing ones, please ask your space admin to upgrade your plan.
+            <a @click="trackClickEvent" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" :href="upgradeUrl" target="_blank">[Learn more about plan limits]</a>
           </p>
         </div>
       </div>
@@ -19,48 +17,47 @@
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
-import getFeatureFlagsForCurrentDomain from "@/apis/featureFlags";
+import {computed, ref} from 'vue';
 import {trackEvent} from "@/utils/window";
-import globals from "@/model/globals";
 import macroMetrics from "@/services/MacroMetrics";
+import getFeatureFlagsForCurrentDomain from "@/apis/featureFlags";
 
-const showNotice = ref(false);
-const numberOfMacros = ref('200');
+// Constants
+const upgradeUrl = 'https://zenuml.com/upgrade/'
+const macrosLimit = 100
+const WARNING_THRESHOLD = 85
 
-function onUpgradeLinkClicked() {
-  trackEvent('upgrade-link', 'click', 'upgrade-notice')
+// State
+const macrosCreated = ref<number>(0)
+const customerSuccessServiceEnabled = ref<boolean>(false)
+
+// Computed
+const actionRequired = computed(() => {
+  return (macrosCreated.value >= WARNING_THRESHOLD) && customerSuccessServiceEnabled.value
+})
+
+// Feature flag handling
+const loadCustomerSuccessServiceFlag = async () => {
+  const customerSuccessService: any = await getFeatureFlagsForCurrentDomain(['CUSTOMER_SUCCESS_SERVICE'])
+  customerSuccessServiceEnabled.value = !!customerSuccessService.CUSTOMER_SUCCESS_SERVICE
+  trackEvent('', customerSuccessServiceEnabled.value ? 'css-enabled' : 'css-disabled', 'conversion')
 }
 
-function onContactLinkClicked() {
-  trackEvent('contact-link', 'click', 'upgrade-notice')
+// Metrics loading
+const loadMacroMetrics = async () => {
+  const metrics = await macroMetrics.getMacroMetrics()
+  if (metrics?.total) {
+    macrosCreated.value = metrics.total
+  }
 }
 
-onMounted(async() => {
-  const isLite = globals.apWrapper.isLite();
-  const macroData = await globals.apWrapper.getMacroData();
-  if (!isLite) {
-    trackEvent(macroData?.uuid, 'skip', 'upgrade-notice')
-    return;
-  }
+// Event tracking
+const trackClickEvent = () => {
+  trackEvent('upgrade', 'click', 'conversion')
+}
 
-  trackEvent(macroData?.uuid, 'get-feature-flags', 'upgrade-notice')
-  const customerSuccessService = await getFeatureFlagsForCurrentDomain(['CUSTOMER_SUCCESS_SERVICE']);
-  // @ts-ignore
-  if (!customerSuccessService.CUSTOMER_SUCCESS_SERVICE) {
-    trackEvent(macroData?.uuid, 'hold', 'upgrade-notice')
-  } else {
-    let metrics = await macroMetrics.getMacroMetrics();
 
-    if(!!metrics?.total && (metrics.total > 85)) {
-      trackEvent(macroData?.uuid, 'show-notice', 'upgrade-notice', { metrics });
-      showNotice.value = true;
-      // @ts-ignore
-      numberOfMacros.value = metrics?.total;
-    } else {
-      trackEvent(macroData?.uuid, 'skip', 'upgrade-notice', { metrics });
-    }
-  }
-});
-
+// Initialize
+loadMacroMetrics();
+loadCustomerSuccessServiceFlag();
 </script>

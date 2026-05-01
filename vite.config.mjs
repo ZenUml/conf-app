fb782d1714c8f40a9aba8302bba1d6781858d803
MrCoder <eagle.xiao@gmail.com>
2026-05-01 12:17:01 +1000

    chore(vitest): exclude .worktrees from test discovery

--- vite.config.mjs
+++ vite.config.mjs
@@ -96,6 +96,7 @@ export default defineConfig({
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
+      '**/.worktrees/**',
       '**/cypress/**',
       '**/.{idea,git,cache,output,temp}/**',
       '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
